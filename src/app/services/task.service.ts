cdac6c8bd2cd62f20fbb67259c0fadd99151a400
Kalindu Sembakutti <sembukkk@mcmaster.ca>
2023-10-07 14:10:50 -0400

    df

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -13,7 +13,7 @@ const httpOptions = {
   providedIn: 'root',
 })
 export class TaskService {
-  private apiUrl = 'http://localhost/tasks';
+  private apiUrl = 'http://192.168.0.22/tasks';
 
   constructor(private http: HttpClient) {}
 
